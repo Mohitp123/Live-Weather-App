d80c691e507db2d150954cc9f55a61901ac3f299
Chaudhary <m.d.chaudhary@accenture.com>
2023-03-20 11:21:30 +0530

    Final code

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "232b78b3b954d9f7df40da40b741159a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
